253b0531e0e4e48a36040792a3e02cc05ebf799b
RIanGillisAlgorithms <odie_135@yahoo.com>
2026-01-02 19:52:01 -0500

    Add files via upload

---
+++ MuertEngine/main.cpp
@@ -0,0 +1,42 @@
+#include "main.h"
+//#include "../MuertDraw/gameState_SETTINGS.h"
+
+
+
+//#define _CRTDBG_MAP_ALLOC
+//#include <stdlib.h>
+//#include <crtdbg.h>
+
+
+//https://randomascii.wordpress.com/2012/02/13/dont-store-that-in-a-float/
+int main()
+{
+	////from https://docs.microsoft.com/en-us/visualstudio/debugger/finding-memory-leaks-using-the-crt-library?view=vs-2019
+	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+
+
+	//The stateHolder constructor inserts GAMESTATE_QUIT and GAMESTATE_CLEANUP by default
+	//Once all of the other gameStates have been removed from the stack it will
+	//go through all of the GAMESTATE_CLEANUP subStates in turn, pop itself off the stack
+	//and then go through all of the GAMESTATE_QUIT subStates before exiting
+	GAME::STATEHOLDER states;
+
+	////////////////////////////////////////////////////////////////////////////////
+	// 
+	// Add the initial game state here
+	// 
+	//		a GAMESTATE #includes it's parentState and forward-declares any childStates
+	// 
+	////////////////////////////////////////////////////////////////////////////////
+
+	//ProgramLoop starts at GAMESTATE_SETTINGS
+	//states.pushState(new GAME::GAMESTATE_SETTINGS());
+
+	//or load from DLL:
+	//auto gsSettings = states.loadStateFromDLL("MuertEngine.State.Draw.dll", "create_GAMESTATE_SETTINGS");
+	//states.pushState(gsSettings);
+
+
+	return states.ProgramLoop();
+}
---
+++ MuertEngine/main.h
@@ -0,0 +1,11 @@
+//by ian.gillis
+#pragma once
+#ifndef MAIN_H
+#define MAIN_H
+//#define _SECURE_SCL 0
+
+#include "../MuertEngine.State/stateHolder.h"
+
+
+
+#endif
